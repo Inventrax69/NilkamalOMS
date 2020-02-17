fc51a38822a540c5c55341f47f7eecc874d0a0b0
Padmaja.B <padmaja.bodsakurthi@inventrax.com>
2020-02-17 19:34:05 +0530

    padamja

--- app/src/main/java/com/example/inventrax/falconOMS/activities/MainActivity.java
+++ app/src/main/java/com/example/inventrax/falconOMS/activities/MainActivity.java
@@ -81,7 +81,6 @@ import com.example.inventrax.falconOMS.util.SharedPreferencesUtils;
 import com.example.inventrax.falconOMS.util.SnackbarUtils;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -89,7 +88,6 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import retrofit2.Call;
@@ -103,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String classCode = "OMS_Android_MainActivity";
     private static final String changedForRef = "OMS_Android_MainActivity";
+    private static final String hi = "OMS_Android_MainActivity";
 
     // this is added for testing
 
